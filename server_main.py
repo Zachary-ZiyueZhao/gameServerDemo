f1c916ce076987e16a385be7c0dedd9283fbb0b3
Zachary-ZiyueZhao <zachary20080717@163.com>
2025-08-06 20:40:04 +0800

    v2 JOIN_ROOM LEAVE_ROOM

--- server_main.py
+++ server_main.py
@@ -1,6 +1,10 @@
 import asyncio, json
 from user_store import verify_login, register_user
 
+rooms = {}
+room_counter = 1000  # 初始房间ID
+
+
 async def handle_client(reader, writer):
     addr = writer.get_extra_info('peername')
     print(f"客户端连接：{addr}")
@@ -31,6 +35,83 @@ async def handle_client(reader, writer):
                 response = {"type": "REGISTER_SUCCESS"}
             else:
                 response = {"type": "REGISTER_FAIL", "msg": "User exists"}
+
+        elif message.get("type") == "CREATE_ROOM":
+            global room_counter
+            username = message.get("username")
+            room_id = str(room_counter)
+            room_counter += 1
+
+            # 创建新房间并加入玩家
+            rooms[room_id] = {
+                "id": room_id,
+                "players": [username],
+                "status": "waiting"
+            }
+
+            response = {
+                "type": "CREATE_ROOM_SUCCESS",
+                "room_id": room_id
+            }
+
+        elif message.get("type") == "LIST_ROOMS":
+            room_list = []
+            for room in rooms.values():
+                room_list.append({
+                    "id": room["id"],
+                    "players": len(room["players"]),
+                    "status": room["status"]
+                })
+
+            response = {
+                "type": "ROOM_LIST",
+                "rooms": room_list
+            }
+
+        elif message.get("type") == "JOIN_ROOM":
+            room_id = message.get("room_id")
+            username = message.get("username")
+
+            if room_id in rooms:
+                if username in rooms[room_id]["players"]:
+                    response = {
+                        "type": "JOIN_ROOM_FAIL",
+                        "msg": "你已经在该房间中"
+                    }
+                else:
+                    rooms[room_id]["players"].append(username)
+                    response = {
+                        "type": "JOIN_ROOM_SUCCESS",
+                        "room_id": room_id,
+                        "players": rooms[room_id]["players"]
+                    }
+            else:
+                response = {
+                    "type": "JOIN_ROOM_FAIL",
+                    "msg": "房间不存在"
+                }
+
+        elif message.get("type") == "LEAVE_ROOM":
+            username = message.get("username")
+            room_id = message.get("room_id")
+
+            if room_id in rooms:
+                room = rooms[room_id]
+                if username in room["players"]:
+                    room["players"].remove(username)
+
+                    # 如果房间没人了，删除房间
+                    if not room["players"]:
+                        del rooms[room_id]
+
+                response = {
+                    "type": "LEAVE_ROOM_SUCCESS"
+                }
+            else:
+                response = {
+                    "type": "LEAVE_ROOM_FAIL",
+                    "msg": "房间不存在"
+                }
 
         writer.write((json.dumps(response) + "\n").encode())
         await writer.drain()
